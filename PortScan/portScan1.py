5661d869a08727e25069ddf4e1dc252a21a8f500
Charith Jayasanka <charithjayasanka2@gmail.com>
2019-09-29 11:34:49 -0400

    added comments

--- PortScan/portScan1.py
+++ PortScan/portScan1.py
@@ -4,14 +4,17 @@
 import socket
 # AF_INET => IPV4 Addresses / Sock_STREAM => TCP Packets
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host = "your_ip_or_host"
-port = #port to be scanned
+host = "<your_ip_or_host>"
+port = <port to be scanned>
 
 def portScanner(port):
 #checks if connection method throws any errors
 	if sock.connect_ex((host,port)):
-		print "port %d is closed" % (port)
+		print "Port %d is closed" % (port)
 #if no error is thrown, the port is open
 	else:
 		print "Port%d is opened" % (port)
 portScanner(port)
+
+#you can also check the open ports using 
+# nmap <your_host_or_ip_to_check>
